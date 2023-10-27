ec900c6fd9a8d1cdcac016b6297fcd288b4043b0
David Asatrian <david.asatrian@gmail.com>
2023-10-27 16:50:18 -0700

    Change image and animation loading to loops

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -1,12 +1,18 @@
+/**
+ * Preloader scene.
+ */
 export class Preloader extends Phaser.Scene {
   constructor() {
     super("Preloader");
 
-    this.loadText;
+    this.loadingText;
   }
 
+  /**
+   * Load all the assets used by the game.
+   */
   preload() {
-    this.loadText = this.add
+    this.loadingText = this.add
       .text(
         this.cameras.main.width / 2,
         this.cameras.main.height / 2,
@@ -18,8 +24,6 @@ export class Preloader extends Phaser.Scene {
         }
       )
       .setOrigin(0.5);
-    // .setStroke("#203c5b", 6)
-    // .setShadow(2, 2, "#2d2d2d", 4, true, false);
 
     this.load.setPath("/assets/images/");
 
@@ -29,56 +33,47 @@ export class Preloader extends Phaser.Scene {
     this.load.image("player-writing", "player-writing.png");
     this.load.image("envelope", "envelope.png");
 
-    this.load.image("walk-1", "walk-1.png");
-    this.load.image("walk-2", "walk-2.png");
-    this.load.image("walk-3", "walk-3.png");
-    this.load.image("walk-4", "walk-4.png");
-    this.load.image("walk-5", "walk-5.png");
-    this.load.image("walk-6", "walk-6.png");
+    for (let i = 1; i <= 6; i++) {
+      this.load.image(`walk-${i}`, `walk-${i}.png`);
+    }
 
-    this.load.image("run-1", "run-1.png");
-    this.load.image("run-2", "run-2.png");
-    this.load.image("run-3", "run-3.png");
-    this.load.image("run-4", "run-4.png");
-    this.load.image("run-5", "run-5.png");
-    this.load.image("run-6", "run-6.png");
-    this.load.image("run-7", "run-7.png");
-    this.load.image("run-8", "run-8.png");
+    for (let i = 1; i <= 8; i++) {
+      this.load.image(`run-${i}`, `run-${i}.png`);
+    }
   }
 
+  /**
+   * Create the animations used by the game and start the Game scene.
+   */
   create() {
     this.anims.create({
       key: "walk",
-      frames: [
-        { key: "walk-1" },
-        { key: "walk-2" },
-        { key: "walk-3" },
-        { key: "walk-4" },
-        { key: "walk-5" },
-        { key: "walk-6" },
-      ],
+      frames: (() => {
+        const frameData = [];
+        for (let i = 1; i <= 6; i++) {
+          frameData.push({ key: `walk-${i}` });
+        }
+        return frameData;
+      })(),
       frameRate: 10,
       repeat: -1,
     });
 
     this.anims.create({
       key: "run",
-      frames: [
-        { key: "run-1" },
-        { key: "run-2" },
-        { key: "run-3" },
-        { key: "run-4" },
-        { key: "run-5" },
-        { key: "run-6" },
-        { key: "run-7" },
-        // { key: "run-8" },
-      ],
+      frames: (() => {
+        const frameData = [];
+        for (let i = 1; i <= 8; i++) {
+          frameData.push({ key: `run-${i}` });
+        }
+        return frameData;
+      })(),
       frameRate: 5,
       repeat: -1,
     });
 
     if (this.sound.locked) {
-      this.loadText.setText("Click to start");
+      this.loadingText.setText("Click to start");
 
       this.input.once("pointerdown", () => {
         this.scene.start("Game");
