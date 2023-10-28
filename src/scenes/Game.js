2743343453ca96c733f9fc3f8f098dd590eac9f9
David Asatrian <david.asatrian@gmail.com>
2023-10-27 17:24:01 -0700

    Add score and highscore

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,12 +10,23 @@ export class Game extends Phaser.Scene {
 
     this.player;
     this.lanes;
+
+    this.score = 0;
+    this.highscore = 0;
+
+    this.scoreLabel;
+    this.scoreText;
+    this.highscoreLabel;
+    this.highscoreText;
   }
 
   /**
    * Create the game scene.
    */
   create() {
+    this.score = 0;
+    this.highscore = this.registry.get("highscore");
+
     this.add.image(0, 0, "background").setOrigin(0, 0);
 
     this.lanes = [
@@ -26,6 +37,30 @@ export class Game extends Phaser.Scene {
     ];
 
     this.player = new Player(this, this.lanes[0]);
+
+    this.scoreLabel = this.add.text(190, -20, "Score:", {
+      fontFamily: "hwMadeline",
+      fontSize: 100,
+      color: "#000000",
+    });
+
+    this.scoreText = this.add.text(340, -20, this.score, {
+      fontFamily: "hwMadeline",
+      fontSize: 100,
+      color: "#000000",
+    });
+
+    this.highscoreLabel = this.add.text(570, -20, "High score:", {
+      fontFamily: "hwMadeline",
+      fontSize: 100,
+      color: "#000000",
+    });
+
+    this.highscoreText = this.add.text(810, -20, this.highscore, {
+      fontFamily: "hwMadeline",
+      fontSize: 100,
+      color: "#000000",
+    });
 
     this.input.keyboard.once("keydown-SPACE", this.start, this);
     this.input.keyboard.once("keydown-UP", this.start, this);
